032152ab6c2459e8756ff683848a758e73d2e865
shivanand733 <shivanandkumbar5604@gmail.com>
2026-01-29 23:35:13 +0530

    WAP to salarry + 1000 bounus and display the details

---
+++ src/S1Jan/salarry.java
@@ -0,0 +1,23 @@
+package S1Jan;
+
+import java.util.Scanner;
+
+public class salarry {
+public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	
+	System.out.println("Enter Number Of the Employee");
+	int NoOFEmp = scan.nextInt();
+	
+	System.out.println("Entr Sallary of Each Employees");
+	int[] arry = new int[NoOFEmp];
+	
+	for (int i = 0; i <= arry.length-1; i++) {
+		 arry[i] = scan.nextInt();
+	}
+	
+	for (int i = 0; i <= arry.length-1; i++) {
+		System.out.println("Bounus 1000 + Sallary is " + (arry[i] + 1000) + " ");
+	}
+}
+}
