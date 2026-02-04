51a335d299752e3f9ae6b928808d5bd18442b1ca
shivanand733 <shivanandkumbar5604@gmail.com>
2026-02-04 20:01:01 +0530

    complete the creation of the arry of the ravi class

--- src/S1Feb/ravi.java
+++ src/S1Feb/ravi.java
@@ -7,6 +7,13 @@ public class ravi {
         System.out.println("Enter the Name");
         int size = scan.nextInt();
         String[] arry = new  String[size];
+
+        for (int i = 0; i <= arry.length-1; i++) {
+            arry[i] = scan.next();
+        }
+        for (int i = 0; i <= arry.length-1; i++) {
+            System.out.println(arry[i]);
+        }
         
     }
 
