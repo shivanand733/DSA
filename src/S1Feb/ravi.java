9fb97eaab45a548ef9846a96c8b55a29f5a49d43
shivanand733 <shivanandkumbar5604@gmail.com>
2026-02-04 19:57:01 +0530

    crate names Arrays

--- src/S1Feb/ravi.java
+++ src/S1Feb/ravi.java
@@ -1,4 +1,13 @@
+import java.util.Scanner;
+
 public class ravi {
 
-    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the Name");
+        int size = scan.nextInt();
+        String[] arry = new  String[size];
+        
+    }
+
 }
