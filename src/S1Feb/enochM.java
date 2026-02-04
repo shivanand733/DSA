8c4471a691bf4dac5301537bc86b894643487760
shivanand733 <shivanandkumbar5604@gmail.com>
2026-02-04 11:18:41 +0530

    create a 2D Arrays

--- src/S1Feb/enochM.java
+++ src/S1Feb/enochM.java
@@ -1,5 +1,30 @@
 package S1Feb;
 
-public class enochM {
+import java.util.Scanner;
 
+public class enochM {
+public static void main(String[] args) {
+	
+	Scanner scan = new Scanner(System.in);
+	System.out.println("Enter the Size of the Rows only");
+	int row = scan.nextInt();
+	int colum = scan.nextInt();
+	
+	int[][] Jarry = new int[row][colum];
+	
+	System.out.println("the size of " + row + " and add the column");
+	for (int j = 0;j <= Jarry.length-1;j++) {
+		for (int i = 0; i <= Jarry[i].length-1;i++) {
+			Jarry[j][i] = scan.nextInt();
+		}
+	}
+	
+	System.out.println("The output of the arrys are ");
+	for (int j = 0; j <= Jarry.length-1; j++) {
+		for (int i = 0; i <= Jarry[i].length-1; i++) {
+			System.out.print(Jarry[j][i] + " ");
+		}
+		System.out.println();
+	}
+}
 }
