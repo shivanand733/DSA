c3e2061f600367a13719fb1bb8a684c106c2b2cd
shivanand733 <shivanandkumbar5604@gmail.com>
2026-02-05 20:14:45 +0530

    create bookApp and book class and take input form the user

--- src/S1Feb/book.java
+++ src/S1Feb/book.java
@@ -2,4 +2,15 @@ package S1Feb;
 
 public class book {
 
+	String bookName;
+	int cost;
+	
+	public book(String bookName, int cost) {
+		// TODO Auto-generated constructor stub
+		this.bookName = bookName;
+		this.cost = cost;
+	}
+	public book() {
+		// TODO Auto-generated constructor stub
+	}
 }
--- src/S1Feb/bookApp.java
+++ src/S1Feb/bookApp.java
@@ -1,7 +1,14 @@
 package S1Feb;
 
+import java.util.Scanner;
+
 public class bookApp {
 public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	
+	System.out.println("Enter the size of the book ");
+	int size = scan.nextInt();
+	book[] arry = new book[size];
 	
 }
 }
