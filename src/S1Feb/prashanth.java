acb527b56e59fb6bde495c97b90390c583eeb4a6
shivanand733 <shivanandkumbar5604@gmail.com>
2026-02-09 23:25:28 +0530

    create a Prashanth projects

---
+++ src/S1Feb/prashanth.java
@@ -0,0 +1,13 @@
+package S1Feb;
+
+import java.util.Scanner;
+
+public class prashanth {
+public static void main(String[] args) {
+	Scanner scan= new Scanner(System.in);
+	
+	System.out.println("Enter the Numnber of the size ");
+	int size = scan.nextInt();
+	
+}
+}
