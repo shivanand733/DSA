9a3e1e3cb8b0cc207f6485f112539c7ec784fab1
shivanand733 <shivanandkumbar5604@gmail.com>
2026-02-20 21:19:15 +0530

    create simple projecrts

---
+++ src/S1Feb/shivMicroNova.java
@@ -0,0 +1,17 @@
+package S1Feb;
+
+public class shivMicroNova {
+
+	int empNo;
+	String name;
+	String department;
+	String depHead;
+	
+	public shivMicroNova() {
+		// default constructor 
+	}
+	
+	public shivMicroNova(int empNo, String name) {
+		
+	}
+}
