f33d4b9f97299089ce86f85ca1bddff7b5c7facb
shivanand733 <shivanandkumbar5604@gmail.com>
2026-02-20 21:39:45 +0530

    create a simple MicroNova Employee projects

--- src/S1Feb/shivMicroNovaApp.java
+++ src/S1Feb/shivMicroNovaApp.java
@@ -1,5 +1,19 @@
 package S1Feb;
 
-public class shivMicroNovaApp {
+import java.util.Scanner;
 
+public class shivMicroNovaApp {
+public static void main(String[] args) {
+	
+	Scanner scan = new Scanner(System.in);
+	System.out.println("Eneter Employee Id Number");
+	System.out.println("Eneter Employee Name");
+	System.out.println("Eneter Employee Department");
+	System.out.println("Eneter Employee Department Head");
+	
+	shivMicroNova shiv = new shivMicroNova(scan.nextInt(), scan.nextLine()+ scan.nextLine(), scan.nextLine(), scan.nextLine());
+	
+	
+	System.out.println(shiv);
+}
 }
