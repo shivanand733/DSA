7a8440b9d844e1739b15ba9ac2b23e408423e7bb
shivanand733 <shivanandkumbar5604@gmail.com>
2026-02-23 23:57:00 +0530

    creat a jagged Arrays

---
+++ src/S1Feb/jagedArray.java
@@ -0,0 +1,23 @@
+package S1Feb;
+
+import java.util.Scanner;
+
+//creating the jagged array
+public class jagedArray {
+public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+
+//	Creating An Arrays
+	int[][] arry = new int[scan.nextInt()][];
+	
+		for (int i = 0; i <= arry[i].length-1; i++) {
+			arry[i] = new int[scan.nextInt()];
+		}
+	
+	for (int j = 0; j <= arry.length-1; j++) {
+		for (int i = 0; i <= arry[i].length-1; i++) {
+			
+		}
+	}
+}
+}
