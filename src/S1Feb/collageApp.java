a098b653bfc72b03c4eb440b990137939bec8f43
shivanand733 <shivanandkumbar5604@gmail.com>
2026-02-23 22:39:46 +0530

    create 2D arrays for String (Names)

--- src/S1Feb/collageApp.java
+++ src/S1Feb/collageApp.java
@@ -1,5 +1,27 @@
 package S1Feb;
 
-public class collageApp {
+import java.util.Scanner;
 
+public class collageApp {
+public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	
+	System.out.println("Eneter the size of the String Arrays");
+
+	String[][] arry = new String[scan.nextInt()][scan.nextInt()];	//Arrays Creation 
+	
+	System.out.println("Enetr The Names");
+	for (int j = 0; j <= arry.length-1; j++) {
+		for (int i = 0; i <= arry[j].length-1; i++) {
+			arry[j][i] = scan.nextLine();
+		}
+	}
+	
+	for (int j = 0; j <= arry.length-1;j++) {
+		for (int i = 0; i <= arry[j].length-1; i++) {
+			System.out.print(arry[j][i] + " ");
+		}
+		System.out.println();
+	}
+}
 }
