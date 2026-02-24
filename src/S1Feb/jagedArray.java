6b4ea8d578dfe18e7979aa9f16932d8e0761ac20
shivanand733 <shivanandkumbar5604@gmail.com>
2026-02-24 22:06:51 +0530

    create a jagged 2D Arrya ex of students of each class toget students marks in each students

--- src/S1Feb/jagedArray.java
+++ src/S1Feb/jagedArray.java
@@ -8,16 +8,27 @@ public static void main(String[] args) {
 	Scanner scan = new Scanner(System.in);
 
 //	Creating An Arrays
+	System.out.println("Enter number of class");
 	int[][] arry = new int[scan.nextInt()][];
 	
-		for (int i = 0; i <= arry[i].length-1; i++) {
+		System.out.println("Enter each class how many  students");
+		for (int i = 0; i <= arry.length-1; i++) {
 			arry[i] = new int[scan.nextInt()];
 		}
 	
+		System.out.println("Enter the Students marks in each class ");
 	for (int j = 0; j <= arry.length-1; j++) {
-		for (int i = 0; i <= arry[i].length-1; i++) {
-			
+		for (int i = 0; i <= arry[j].length-1; i++) {
+			arry[j][i] = scan.nextInt();
 		}
+	}
+	
+	System.out.println("The Each class students marks");
+	for (int j = 0; j <= arry.length-1; j++) {
+		for (int i = 0; i <= arry[j].length-1; i++) {
+			System.out.print(arry[j][i] + " ");
+		}
+		System.out.println(); 	// this is for next line 
 	}
 }
 }
