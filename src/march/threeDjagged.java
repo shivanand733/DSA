054054d3e438b3858e268f9d6d5aa69e648387f3
shivanand733 <shivanandkumbar5604@gmail.com>
2026-03-05 21:31:59 +0530

    create a 3D Jagged Array, how to actually 3D arry work

---
+++ src/march/threeDjagged.java
@@ -0,0 +1,16 @@
+package march;
+
+import java.util.Scanner;
+
+public class threeDjagged {
+public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	
+//	3D Array Creation 
+	int [][][] arry = new int[3][][];
+	
+	 arry[0] = new int[3][];
+	 arry[1] = new int[2][];
+	 arry[2] = new int[4][];
+}
+}
