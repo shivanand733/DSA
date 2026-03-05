424fb35f35fe0c813bbda5751d1d181fdf305078
shivanand733 <shivanandkumbar5604@gmail.com>
2026-03-05 23:22:19 +0530

    crate 3D Jagged Array

--- src/march/threeDjagged.java
+++ src/march/threeDjagged.java
@@ -12,5 +12,27 @@ public static void main(String[] args) {
 	 arry[0] = new int[3][];
 	 arry[1] = new int[2][];
 	 arry[2] = new int[4][];
+	 
+	 arry[0][3] = new int[5];
+	 arry[1][2] = new int[3];
+	 arry[3][4] = new int[4];
+	 
+	 for (int i = 0; i <= arry.length-1; i++) {
+		 for (int j = 0; j <= arry[i].length-1; j++) {
+			 for (int k = 0; k <= arry[i][j].length-1;k++) {
+				 arry[i][j][k] = scan.nextInt();
+			 }
+		 }
+	 }
+	 
+	 for (int i = 0; i <= arry.length-1; i++) {
+		 for (int j = 0; j<= arry[i].length-1; j++) {
+			 for (int k = 0; k <= arry[i][j].length-1; k++) {
+				 System.out.println(arry[i][j][k] + " ");
+			 }
+			 System.out.println();   	//the Line breaker
+		 }
+		 System.out.println("----------------------------------------------"); //one complete loop is complete..
+	 }
 }
 }
