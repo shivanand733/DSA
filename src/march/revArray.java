fbfd5b15f7881d10085e1cbb9cc9c5ab5c88cc00
shivanand733 <shivanandkumbar5604@gmail.com>
2026-03-11 23:14:42 +0530

    create Sum of the Arrays

---
+++ src/march/revArray.java
@@ -0,0 +1,24 @@
+package march;
+
+import java.util.Scanner;
+
+public class revArray {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter Size of the Arrays");
+//		Array Creation
+		int [] arry = new int[scan.nextInt()];
+		int sum = 0;
+		for (int i = 0; i<= arry.length-1; i++) {
+			System.out.println("Enetr the Array "  + i);
+			arry[i] = scan.nextInt();
+		}
+		
+		for (int i = 0; i <= arry.length-1; i++) {
+			sum += arry[i];
+		}
+		
+		System.out.println("The Sum of the Arrys Are : " + sum);
+	}
+}
