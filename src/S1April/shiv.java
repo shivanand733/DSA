cbdf39558496004b837588092198a2b7a3286471
shivanand733 <shivanandkumbar5604@gmail.com>
2026-04-20 21:35:45 +0530

    WAP to find the maximum in each 2D Layer In 3D Array

--- src/S1April/shiv.java
+++ src/S1April/shiv.java
@@ -10,6 +10,7 @@ public static void main(String[] args) {
 	System.out.println("Enter The Size of Arrys");
 	int arry[][][] = new int[scan.nextInt()][scan.nextInt()][scan.nextInt()];
 	
+	System.out.println("Enter the numbers");
 	for (int i = 0; i <= arry.length-1; i++) {
 		for (int j  = 0; j <= arry[i].length-1; j++) {
 			for (int k = 0; k <= arry[i][j].length-1; k++) {
@@ -17,6 +18,34 @@ public static void main(String[] args) {
 			}
 		}
 	}
+	
+	System.out.println("3D Array: ");
+	for (int i = 0; i <= arry.length-1;i++) {
+		System.out.println("Layer " + (i+1) + ":");
+		for (int j = 0; j <= arry[i].length-1;j++) {
+			for (int k = 0; k <= arry[i][j].length-1;k++) {
+				System.out.print(arry[i][j][k] + " ");
+			}
+			System.out.println();	//each 2 number next line 
+		}
+		System.out.println(); //for next line 
+	}
+	
+	for (int i = 0; i<= arry.length-1; i++) {
+		int max = arry[i][0][0];
+		
+		
+		for(int j = 0; j <= arry[i].length-1;j++) {
+			for (int k = 0; k <= arry[i][j].length-1; k++) {
+				if (arry[i][j][k] > max) {
+					max = arry[i][j][k];
+				}
+			}
+		}
+		
+		System.out.println("Maximun value in layer " + (i+1) + ": " + max);
+	}
+	
 	
 
 }
