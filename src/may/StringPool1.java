7bec31f527983f91794fc1d0f1edf3eb72b0cd08
shivanand733 <shivanandkumbar5604@gmail.com>
2026-05-15 07:52:56 +0530

    String Pool concept

---
+++ src/may/StringPool1.java
@@ -0,0 +1,27 @@
+package may;
+
+public class StringPool1 {
+public static void main(String[] args) {
+	
+	String s1 = "Kod";
+	String s2 = "Nest";
+	String s3 = "kod" + "Nest";
+	String s4 = "kod" + "Nest";
+	String s5 = s1+ s2;
+	String s6 = s1+ s2;
+	
+	if (s3 == s4) {
+		 System.out.println("The refernces Are Same");
+	} else {
+		System.out.println("The Reference Are not Same..");
+	}
+	
+	System.out.println("________________________________________");
+	
+	if (s3.equals(s4)) {
+		System.out.println("The Contet Willl be Same");
+	} else  {
+		System.out.println("The Contet not match Or Not Same");
+	}
+}
+}
