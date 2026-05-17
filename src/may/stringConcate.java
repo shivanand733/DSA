4ff28289536dfde16651bfa66aab97dd9875df29
shivanand733 <shivanandkumbar5604@gmail.com>
2026-05-17 21:53:26 +0530

    String Concatination

---
+++ src/may/stringConcate.java
@@ -0,0 +1,15 @@
+package may;
+
+import java.util.Scanner;
+
+public class stringConcate {
+public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	String str1 = "Kodnest";
+
+	System.out.println(str1);
+	
+	str1.concat("Tech");
+	System.out.println(str1);
+}
+}
