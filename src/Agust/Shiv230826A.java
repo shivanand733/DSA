6f86e31df6d6749427b03ddac7f2db23d2575757
shivanand733 <shivanandkumbar5604@gmail.com>
2026-08-23 20:09:31 +0530

    Create Using The StringBuilder Same Example in preivious Programs,

---
+++ src/Agust/Shiv230826A.java
@@ -0,0 +1,21 @@
+package Agust;
+import java.util.Scanner;
+
+public class Shiv230826A {
+	public static void main(String Args[]) {
+
+		new DemoBilder().Message();
+	}
+	
+	public static class DemoBilder {
+		void Message(){
+		StringBuilder sb1 = new StringBuilder();
+		
+		sb1.append("Hello");
+		sb1.append(" Java Learners");
+		sb1.append(" Welcome back....");
+		
+		System.out.println("The String Builder using Appends Are " + sb1);
+		}
+	}
+}
