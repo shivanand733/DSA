5df2cdfd29d280e5e80713557c3b309d3fe2414a
shivanand733 <shivanandkumbar5604@gmail.com>
2026-09-06 21:42:20 +0530

    Create the String Uppercase of the Each 1st later and remaing Later will be the  Lower case

--- src/Agust/Demo3.java
+++ src/Agust/Demo3.java
@@ -1,12 +1,24 @@
 package Agust;
 
+import java.util.Scanner;
+
 public class Demo3 {
 public static void main(String[] args) {
-	String s1 = "Kodnest";
+	Scanner scan = new Scanner(System.in);
 	
-	System.out.println(s1);
-//	s1.concat("Academy"); //Strings Are Immutable 
-	s1=s1.concat("Academy");
-	System.out.println(s1);
+	System.out.println("Enter String: ");
+	String str1 = scan.nextLine();
+	
+	String[] word = str1.split(" ");
+	String result = " ";
+	
+	for (int i = 0; i < word.length;i++) {
+		result = result + Character.toUpperCase(word[i].charAt(0)) 
+		+ word[i].substring(1).toLowerCase() + " ";
+	}
+	
+	System.out.println("The Original String: " + str1);
+	System.out.println("The Title Case: " + result);
+	
 }
 }
