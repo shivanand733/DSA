06ba55247ee9e2b9817012d89bb8afa978157714
shivanand733 <shivanandkumbar5604@gmail.com>
2026-09-06 23:10:56 +0530

    String rotational String

--- src/Agust/Demo3.java
+++ src/Agust/Demo3.java
@@ -4,7 +4,12 @@ import java.util.Scanner;
 
 public class Demo3 {
 public static void main(String[] args) {
-	Scanner scan = new Scanner(System.in);
+//	shiv();
+	shiv1();
+}
+
+public static  void shiv() {
+Scanner scan = new Scanner(System.in);
 	
 	System.out.println("Enter String: ");
 	String str1 = scan.nextLine();
@@ -19,6 +24,22 @@ public static void main(String[] args) {
 	
 	System.out.println("The Original String: " + str1);
 	System.out.println("The Title Case: " + result);
+
+}
+
+public static void shiv1() {
+	Scanner scan = new Scanner(System.in);
+	
+	System.out.println("Enetr any two strings");
+	String str1 = scan.nextLine();
+	String str2 = scan.nextLine();
+	
+	if(str1.length() == str2.length()  && (str1 + str1).contains(str2)) {
+		System.out.println("The string " + str2 + " is a rotation of " + str1);
+	} else {
+		System.out.println("The string " + str2 + " is Not a rotation of " + str1);
+	}
 	
 }
+
 }
