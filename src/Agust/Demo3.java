408dca03fa04491a03559634020911c4d3b9844f
shivanand733 <shivanandkumbar5604@gmail.com>
2026-09-09 08:38:10 +0530

    Practice with Strings Examples

--- src/Agust/Demo3.java
+++ src/Agust/Demo3.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class Demo3 {
 public static void main(String[] args) {
 //	shiv();
-	shiv1();
+//	shiv1();
+//	shiv2();
+	redex001();
 }
 
 public static  void shiv() {
@@ -40,6 +42,40 @@ public static void shiv1() {
 		System.out.println("The string " + str2 + " is Not a rotation of " + str1);
 	}
 	
+}
+
+public static void shiv2() {
+	
+	Scanner scan = new Scanner(System.in);
+	System.out.println("Eneter string");
+	String str1 = scan.nextLine();
+	
+	for (int i = 0; i <= str1.length()-1; i++) {
+		
+		int rep = 0;
+		for (int j = 0; j <= str1.length()-1; j++) {
+			if (str1.charAt(i) == str1.charAt(j)) {
+				rep++;
+			}
+		}
+		System.out.println(str1.charAt(i) + ": "+ rep + ",  ");
+		
+	}
+}
+
+public static void redex001() {
+	Scanner scan = new Scanner(System.in);
+	
+	System.out.println("Enter String");
+	String str = scan.nextLine();
+	
+	String rex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+	
+	if(str.matches(rex)) {
+		System.out.println("Valied String");
+	} else {
+		System.out.println("Invalied String");
+	}
 }
 
 }
