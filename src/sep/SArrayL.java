95ff30cd083637db0ebe4fdcdd7bc285c414f438
shivanand733 <shivanandkumbar5604@gmail.com>
2026-09-17 22:55:48 +0530

    create String ArrayList

--- src/sep/SArrayL.java
+++ src/sep/SArrayL.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 
 public class SArrayL {
 public static void main(String[] args) {
-	AddArryList();
+//	AddArryList();
+	StringArray();
 }
 
 public static void AddArryList() {
@@ -38,4 +39,31 @@ public static void AddArryList() {
 		}
 		
 }
+
+	public static void StringArray() {
+		Scanner scan = new Scanner(System.in);
+		
+		//String ArrayList Creation  
+		ArrayList<String> sh = new ArrayList<String>();
+		
+		System.out.println("Enter the Number");
+		int n = scan.nextInt();
+		scan.nextLine(); //consume nextLine
+		
+		System.out.println("Enetr Names");
+//		Store in the String arrays
+		for (int i = 0; i < n; i++) {
+			sh.add(scan.nextLine());
+		}
+		
+//	Print String Array 
+		
+		System.out.println("Names Are: ");
+		for (int i = 0; i < sh.size(); i++) {
+			System.out.print(sh.get(i) + " ");
+		}
+		
+		
+		
+	}
 }
