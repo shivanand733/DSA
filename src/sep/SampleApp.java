673840ceeaded5b4f18f8ef489073873427e3263
shivanand733 <shivanandkumbar5604@gmail.com>
2026-09-19 23:10:33 +0530

    this program for normal linklist

--- src/sep/SampleApp.java
+++ src/sep/SampleApp.java
@@ -1,7 +1,13 @@
 package sep;
 
+import java.util.Scanner;
+
 public class SampleApp {
 public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in); 
+	Smaple sp = new Smaple();
 	
+	sp.setData(scan.nextLine());
+	System.out.println(sp.getData());
 }
 }
