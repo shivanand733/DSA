85d5548f9fb8512d885c8a8e1f6241c5d43a256d
shivanand733 <shivanandkumbar5604@gmail.com>
2026-09-22 08:38:28 +0530

    create a HashSet Example

---
+++ src/sep/S_hash01.java
@@ -0,0 +1,23 @@
+package sep;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class S_hash01 {
+public static void main(String[] args) {
+	Scanner Scan = new Scanner(System.in);
+	
+//	create Hash List 
+	HashSet hs = new HashSet();
+	hs.add(100);
+	hs.add(50);
+	hs.add(150);
+	hs.add(25);
+	hs.add(75);
+	hs.add(125);
+	hs.add(175);
+	
+	System.out.println("HashSet Outout: " + hs);
+	
+}
+}
