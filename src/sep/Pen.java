a7399aad4b21c4fd9e9179ce68ea45410e24482f
shivanand733 <shivanandkumbar5604@gmail.com>
2026-09-24 07:49:41 +0530

    create Equals Methode Over write

--- src/sep/Pen.java
+++ src/sep/Pen.java
@@ -5,13 +5,13 @@ public class Pen {
 	int id;
 	String color;
 	
-//	@Override
-//	public boolean equals(Object obj) {
-//		Pen p = (Pen) obj;
-//		
-//		if (this.id == p.id && this.color == p.color) 
-//			return true;
-//		else 
-//			return false;
-//	}
+	@Override
+	public boolean equals(Object obj) {
+		Pen p = (Pen) obj;
+		
+		if (this.id == p.id && this.color == p.color) 
+			return true;
+		else 
+			return false;
+	}
 }
